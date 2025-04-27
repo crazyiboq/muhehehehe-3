ff8dec05e263ad63104e217d56fa9750c57d4058
USER <USER@CRAZYIBOQ130420>
2025-04-27 15:42:46 +0400

    Add project files.

---
+++ TCP task manager (Client side)/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var client  = new TcpClient();
+client.Connect(ip, port);
+
+var stream = client.GetStream();
+var bw = new BinaryWriter(stream);
+var br = new BinaryReader(stream);
+
+Command command = null!;
+
+string responce = null!;
+
+string str = null!;
+
+while (true)
+{
+    Console.WriteLine("Write a command name or HELP");
+    str = Console.ReadLine()!.ToUpper();
+    if (str == "HELP")
+    {
+        Console.WriteLine();
+        Console.WriteLine("COMMANDS:");
+        Console.WriteLine(Command.ProcessList);
+        Console.WriteLine($"{Command.Run}  <process name>");
+        Console.WriteLine($"{Command.Kill}  <process name>");
+        Console.ReadLine();
+        Console.Clear();
+    }
+    var input = str.Split(' ');
+    switch (input[0])
+    {
+        case Command.ProcessList:
+            command = new Command { Text = Command.ProcessList };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            var processlist = JsonSerializer.Deserialize<List<string>>(responce);
+            processlist.ForEach(x => Console.WriteLine($"            {x}"));
+
+            break;
+        case Command.Run:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            Console.WriteLine(responce);
+
+            break;
+        case Command.Kill:
+            if (input.Length < 2)
+            {
+                Console.WriteLine("Please provide a process name to kill.");
+                break;
+            }
+            command = new Command { Text = Command.Kill, Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            Console.WriteLine(responce);
+            break;
+
+    }
+    Console.WriteLine("Press any key to continue");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+class Command
+{
+    public const string ProcessList = "PROCLIST";
+    public const string Kill = "KILL";
+    public const string Run = "RUN";
+
+    public string? Text { get; set; }
+    public string? Param { get; set; }
+}
---
+++ TCP task manager (Server side)/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text.Json;
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listener = new TcpListener(ip, port);
+listener.Start();
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    var bw = new BinaryWriter(stream);
+    var br = new BinaryReader(stream);
+
+    while (true)
+    {
+    var input = br.ReadString();
+    Console.WriteLine(input);
+    var command = JsonSerializer.Deserialize<Command>(input);
+
+        switch (command!.Text)
+        {
+            case Command.ProcessList:
+                var processes = Process.GetProcesses();
+                var processNames = JsonSerializer.Serialize(processes.Select(p => p.ProcessName));
+                bw.Write(processNames);
+                break;
+            case Command.Kill:
+                try
+                {
+                    var processesToKill = Process.GetProcessesByName(command.Param);
+
+                    if (processesToKill.Length == 0)
+                    {
+                        bw.Write($"No process with name '{command.Param}' found.");
+                    }
+                    else
+                    {
+                        foreach (var proc in processesToKill)
+                        {
+                            proc.Kill();
+                        }
+                        bw.Write($"Killed {processesToKill.Length} instance(s) of '{command.Param}'.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    bw.Write($"Failed to kill process: {ex.Message}");
+                }
+                break;
+
+            case Command.Run:
+                var process = Process.Start(command.Param);
+                bw.Write($"{process.ProcessName} is started");
+                break;
+            default:
+                Console.WriteLine("Unknown command");
+                break;
+        }
+    }
+}
+class Command
+{
+    public const string ProcessList = "PROCLIST";
+    public const string Kill = "KILL";
+    public const string Run = "RUN";
+
+    public string? Text { get; set; }
+    public string? Param { get; set; }
+}
